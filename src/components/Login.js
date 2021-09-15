6a565b78dc79c893c93168d11a3c4035ca7cfb1f
VelOuria <sofleos@gmail.com>
2021-09-14 22:28:38 -0500

    login responsive

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,38 +5,63 @@ import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import InputAdornment from "@material-ui/core/InputAdornment";
-import FormLabel from "@material-ui/core/FormLabel";
 import Button from "@material-ui/core/Button";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
 import MailOutlineIcon from "@material-ui/icons/MailOutline";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import bgImage from "../assets/login5.jpg";
 const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    flexWrap: "nowrap",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundImage: `url(${bgImage})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    //pendiente: ajustar
+    height: theme.spacing(106),
+    padding: theme.spacing(1),
+
+    [theme.breakpoints.between("sm", "md")]: {
+      height: theme.spacing(130)
+    },
+    [theme.breakpoints.between("lg")]: {
+      // marginTop: theme.spacing(3),
+      height: theme.spacing(140)
+    }
+  },
+  background: {
+    backgroundImage: `url(${bgImage})`
+  },
   title: {
-    fontSize: 24,
-    padding: theme.spacing(0.5),
+    fontSize: 18,
+    marginBottom: theme.spacing(8),
+    padding: theme.spacing(1),
     textAlign: "center",
     backgroundColor: "#FF6347",
     color: "white"
   },
   formControl: {
-    margin: theme.spacing(20),
-    minWidth: 120,
-    marginRight: theme.spacing(200),
-    padding: theme.spacing(2),
-    backgroundColor: "aliceblue",
-
-    "& > *": {
-      margin: theme.spacing(1)
+    //margin: theme.spacing(50),
+    minWidth: 140,
+    padding: theme.spacing(8),
+    backgroundColor: "Snow",
+    borderRadius: 10,
+    position: "relative",
+    [theme.breakpoints.between("sm", "md")]: {
+      width: 480,
+      marginRight: theme.spacing(35),
+      padding: theme.spacing(10)
+    },
+    [theme.breakpoints.between("md", "lg")]: {
+      marginLeft: theme.spacing(90)
     }
   },
-  selectEmpty: {
-    marginTop: theme.spacing(1)
-  },
+
   textField: {
-    marginTop: theme.spacing(2),
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    width: "25ch",
+    marginBottom: theme.spacing(1),
+    minWidth: "25ch",
     display: "flex",
     flexDirection: "row"
   }
@@ -44,50 +69,53 @@ const useStyles = makeStyles(theme => ({
 function Login() {
   const classes = useStyles();
   return (
-    <form className={classes.formControl}>
-      <Card className={classes.title} variant="outlined">
-        <Typography variant="h3">Login</Typography>
-      </Card>
+    <div className={classes.root}>
+      {/* <Paper className={classes.background}> */}
+      <form className={classes.formControl}>
+        <Card className={classes.title} variant="outlined">
+          <Typography variant="h3">Login</Typography>
+        </Card>
 
-      <Grid item className={classes.textField}>
-        <TextField
-          className={classes.textField}
-          id="1"
-          label="Correo electrónico"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <MailOutlineIcon />
-              </InputAdornment>
-            )
-          }}
-        />
-      </Grid>
+        <Grid item className={classes.textField}>
+          <TextField
+            className={classes.textField}
+            id="1"
+            label="Correo electrónico"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <MailOutlineIcon />
+                </InputAdornment>
+              )
+            }}
+          />
+        </Grid>
 
-      <Grid item>
-        <TextField
-          className={classes.textField}
-          id="2"
-          label="Contraseña"
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <LockOutlinedIcon />
-              </InputAdornment>
-            )
-          }}
-        />
-      </Grid>
-      <ButtonGroup
-        orientation="vertical"
-        color="primary"
-        aria-label="vertical contained primary button group"
-        variant="text"
-      >
-        <Button>¿Olvidaste tu contraseña?</Button>
-        <Button>ENTRAR</Button>
-      </ButtonGroup>
-      {/* <InputLabel htmlFor="age-native-simple">
+        <Grid item>
+          <TextField
+            className={classes.textField}
+            id="2"
+            label="Contraseña"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <LockOutlinedIcon />
+                </InputAdornment>
+              )
+            }}
+          />
+        </Grid>
+        <ButtonGroup
+          orientation="vertical"
+          color="primary"
+          aria-label="vertical contained primary button group"
+          variant="text"
+          fullWidth
+        >
+          <Button size="large">ENTRAR</Button>
+          <Button size="small">¿Olvidaste tu contraseña?</Button>
+        </ButtonGroup>
+        {/* <InputLabel htmlFor="age-native-simple">
           <Input id="1" placeholder="correo electrónico" />
         </InputLabel>
       </FormControl>
@@ -95,7 +123,8 @@ function Login() {
         <InputLabel htmlFor="password-native-simple">
           <Input id="2" placeholder="contraseña" />
         </InputLabel> */}
-    </form>
+      </form>
+    </div>
   );
 }
 
