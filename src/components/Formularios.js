7cda424a31e45efa0716aa4e998c1133fce2c892
VelOuria <sofleos@gmail.com>
2021-10-08 12:59:46 -0500

    addedquiz instructions & title

--- src/components/Formularios.js
+++ src/components/Formularios.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import axios from "axios";
 import { MyContext } from "../services/Context";
+import { Typography, Box } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import bgImage from "../assets/dashboard.jpg";
 const useStyles = makeStyles(theme => ({
@@ -15,6 +16,20 @@ const useStyles = makeStyles(theme => ({
     [theme.breakpoints.up("lg")]: {
       height: theme.spacing(140)
     }
+  },
+  title: {
+    textAlign: "center",
+    backgroundColor: "#FF6347",
+    color: "white",
+    fontSize: "1.7rem",
+    borderRadius: 4,
+    padding: theme.spacing(1),
+    [theme.breakpoints.up("lg")]: {
+      fontSize: "2rem"
+    },
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "1.2rem"
+    }
   }
 }));
 function Formularios(props) {
@@ -23,7 +38,8 @@ function Formularios(props) {
   const { changePlace } = useContext(MyContext);
   const user = JSON.parse(localStorage.getItem("USER"));
   const token = user.token;
-
+  const [sections, setSections] = useState([]);
+  const [formData, setFormData] = useState({});
   useEffect(
     () => {
       changePlace("auth");
@@ -35,7 +51,9 @@ function Formularios(props) {
           }
         )
         .then(({ data }) => {
-          console.log(data);
+          const sections = data.result.secciones;
+          setSections(sections);
+          setFormData(data.result);
         })
         .catch(err => {
           console.log(err);
@@ -46,7 +64,15 @@ function Formularios(props) {
 
   return (
     <div className={classes.root}>
-      <p>HOOOLIIIII</p>
+      <Typography variant="h3" className={classes.title}>
+        {formData.nombreFormulario}
+      </Typography>
+      <Box sx={{ mt: 2 }}>
+        <Typography variant="overline">Instrucciones:</Typography>
+        <Typography variant="body1">
+          {formData.indicaciones}
+        </Typography>
+      </Box>
     </div>
   );
 }
